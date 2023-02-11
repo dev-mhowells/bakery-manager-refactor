30910dc3977a2adc9037d241506dadb387207d7f
Mike <dev.mhowells@gmail.com>
2023-02-11 22:50:44 +0000

    comeplted basic restyling of home page for admin users

--- frontend/src/BakeryHome.js
+++ frontend/src/BakeryHome.js
@@ -1,61 +1,52 @@
 import ClearLocalStorage from "./logout";
+import { Link } from "react-router-dom";
 
 export default function BakeryHome() {
   return (
     <div className="flex items-center justify-center h-screen">
-      {/* modal */}
-
-      {/* The button to open modal
-      <label htmlFor="my-modal" className="btn">
-        open modal
-      </label> */}
-
-      {/* Put this part before </body> tag */}
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label htmlFor="my-modal" className="btn">
-              Yay!
-            </label>
+      <div className="w-10/12 h-10/12 rounded-md bg-bone flex justify-center border-8 border-green p-2">
+        {/* modal */}
+        <input type="checkbox" id="my-modal" className="modal-toggle" />
+        <div className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">
+              Congratulations random Internet user!
+            </h3>
+            <p className="py-4">
+              You've been selected for a chance to get one year of subscription
+              to use Wikipedia for free!
+            </p>
+            <div className="modal-action">
+              <label htmlFor="my-modal" className="btn">
+                Yay!
+              </label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src="logoBM8.png" alt="logo" class="rounded-full" />
-        </figure>
-        <div className="card-body items-center text-center">
-          {/* <h2 className="card-title">Shoes!</h2>
+        <div className="card w-96 shadow-xl bg-darkgreen">
+          <figure className="px-10 pt-10">
+            <img src="logoBM8.png" alt="logo" class="rounded-full" />
+          </figure>
+          <div className="card-body items-center text-center bg-darkgreen">
+            {/* <h2 className="card-title">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p> */}
-          <div className="card-actions">
-            <button class="btn bg-beige text-bone btn-block">
-              <a href="/addItem">Add more items</a>
-            </button>
-            <label for="my-modal" class="btn bg-beige text-bone btn-block">
-              Upcoming orders
-            </label>
-            <label
-              //   htmlFor="my-modal"
-              className="btn text-bone bg-beige btn-block"
-            >
-              <a href="/">Home page</a>
-            </label>
-            <label
-              //   htmlFor="my-modal"
-              className="btn text-bone bg-beige btn-block"
-              onClick={ClearLocalStorage()}
-            >
-              <a href="/login">Logout</a>
-            </label>
+            <div className="card-actions">
+              <Link to="/addItem" className="btn-block">
+                <button class="btn bg-beige text-bone btn-block">
+                  Add more items
+                </button>
+              </Link>
+              <label for="my-modal" class="btn bg-beige text-bone btn-block">
+                Upcoming orders
+              </label>
+              <Link to="/" className="btn text-bone bg-beige btn-block">
+                <label>Home page</label>
+              </Link>
+              <Link to="/login" className="btn text-bone bg-beige btn-block">
+                <label onClick={ClearLocalStorage()}>Logout</label>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
