e232778f7484a1c9c327beecac664eb86a7eb5ae
Mike <dev.mhowells@gmail.com>
2023-02-12 13:03:00 +0000

    removed unused code, console logs and commented out code from index.js backend

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,10 @@ const basketRouter = require("./routes/basket");
 const batchOrdersRouter = require("./routes/batchOrders");
 const bakersRouter = require("./routes/bakers");
 const usersRouter = require("./routes/users");
-// const JWT = require("jsonwebtoken");
 const tokensRouter = require("./routes/tokens");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-console.log(process.env.USERNAME);
-console.log(process.env.PASSWORD);
-console.log(process.env.DATABASE);
 
 // Set up mongoose connection
 mongoose.set("strictQuery", false);
@@ -25,44 +20,14 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// middleware function to check for valid tokens
-// const tokenChecker = (req, res, next) => {
-//   let token;
-//   const authHeader = req.get("Authorization");
-
-//   if (authHeader) {
-//     token = authHeader.slice(7);
-//   }
-
-//   JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(401).json({ message: "auth error" });
-//     } else {
-//       req.user_id = payload.user_id;
-//       next();
-//     }
-//   });
-// };
-
 // route setup
 app.use(express.json());
 app.use("/items", itemsRouter);
-// app.use("/orders",tokenChecker,ordersRouter);
 app.use("/orders", basketRouter);
 app.use("/batchOrders", batchOrdersRouter);
 app.use("/bakers", bakersRouter);
 app.use("/users", usersRouter);
 app.use("/tokens", tokensRouter);
-
-// main().catch((err) => console.log(err));
-// async function main() {
-//   await mongoose.connect(mongoDB);
-// }
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Expressingssi!" });
-});
 
 module.exports = app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
