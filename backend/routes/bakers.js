872213a599b244686a9576a78b7b03f118345e0f
Mike <dev.mhowells@gmail.com>
2023-02-12 13:07:53 +0000

    removed all unused routes and controllers related to bakers

--- backend/routes/bakers.js
+++ backend/routes/bakers.js
@@ -3,9 +3,6 @@ const router = express.Router();
 
 const BakersController = require("../controllers/bakers");
 
-router.get("/", BakersController.getAll);
-router.get("/:id", BakersController.getBakerById);
-router.post("/", BakersController.createBaker);
 router.get("/getOrders", BakersController.getOrders);
 
 module.exports = router;
