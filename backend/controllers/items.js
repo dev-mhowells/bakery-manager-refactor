7c56a8628c0c30cf4ebdee3d45c8edca6956e106
Mike <dev.mhowells@gmail.com>
2023-02-12 13:44:27 +0000

    removed route and controller for items - getItemByName as it was unused

--- backend/controllers/items.js
+++ backend/controllers/items.js
@@ -11,10 +11,10 @@ const ItemsController = {
       res.status(200).json({ items: items });
     });
   },
+
   createItem: (req, res) => {
-    console.log("POST ITEM");
     const item = new Item(req.body);
-    console.log("NEW ITEM: ", item);
+
     item.save(async (err) => {
       if (err) {
         throw err;
@@ -24,49 +24,13 @@ const ItemsController = {
     });
   },
 
-  // getImage: (req, res) => {
-  //   getDownloadURL(ref(storage, 'bpcbgbeesroom.png'))
-  //     .then((url) => {
-  //       console.log(url)
-  //       res.status(200).json({ url })
-
-  //     })
-  //     .catch((error) => {
-  //       // Handle any errors
-  //       console.log(error)
-  //     });
-  // },
-  // postImage: (req, res) => {
-  //   try {
-  //     const image = req.body;
-  //     console.log('image', image);
-  //     res.status(200).json({ message: "success" })
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-
-  //   const storage = getStorage();
-  //   const storageRef = ref(storage, 'images');
-
-  //   // 'file' comes from the Blob or File API
-  //   uploadBytes(storageRef, image).then((snapshot) => {
-  //     console.log('Uploaded a blob or file!');
-  //   });
-  // },
-  getItemByName: async (req, res) => {
-    const filter = { itemName: req.params.name };
-    const item = await Item.find(filter).populate().exec();
-    res.status(200).json(item);
-  },
-
   deleteItem: async (req, res) => {
-    console.log("DELETE RAN");
     await Item.deleteOne({ _id: req.body.id });
     const newItems = await Item.find();
     res.status(201).json({ items: newItems });
   },
+
   editItem: async (req, res) => {
-    console.log("this is body", req.body);
     await Item.findByIdAndUpdate(req.body.id, {
       ...req.body.updatedItem,
     });
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -5,9 +5,6 @@ const ItemsController = require("../controllers/items");
 
 router.get("/", ItemsController.getAll);
 router.post("/", ItemsController.createItem);
-// router.get('/getImage', ItemsController.getImage);
-// router.post('/postImage', ItemsController.postImage);
-router.get("/:name", ItemsController.getItemByName);
 router.post("/deleteItem", ItemsController.deleteItem);
 router.post("/editItem", ItemsController.editItem);
 
