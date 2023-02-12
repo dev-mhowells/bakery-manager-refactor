68d7e61d48587056998fc9a75662b768647dbb27
Mike <dev.mhowells@gmail.com>
2023-02-12 13:51:39 +0000

    removed unused controller and route related to users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,24 +2,20 @@ const User = require("../models/user");
 const Basket = require("../models/basket");
 
 const UsersController = {
-  getAll: (req, res) => {
-    User.find(async (err, users) => {
-      if (err) {
-        console.log(err)
-        throw err;
-      }
-      res.status(200).json({ users:  users });
-    });
-  },
   Create: (req, res) => {
     //Create a new basket for the user
-    const newBasket = new Basket ({companyName: req.body.companyName, order: [], dateOfOrder:"", dateRequired :""})
+    const newBasket = new Basket({
+      companyName: req.body.companyName,
+      order: [],
+      dateOfOrder: "",
+      dateRequired: "",
+    });
     newBasket.save(async (err) => {
       if (err) {
         throw err;
       }
-    })
-      
+    });
+
     const user = new User({
       companyName: req.body.companyName,
       email: req.body.email,
@@ -27,48 +23,23 @@ const UsersController = {
       address: req.body.address,
       phoneNumber: req.body.phone_number,
       typeOfBusiness: req.body.typeOfBusiness,
-      currentBasketID: newBasket._id
+      currentBasketID: newBasket._id,
     });
 
     user.save((err) => {
       if (err) {
         res.status(400).json({ message: "Bad request" });
       } else {
-        res.status(201).json({ message: "OK" })
+        res.status(201).json({ message: "OK" });
       }
     });
   },
+
   getUserByID: async (req, res) => {
     const filter = { _id: req.params.userID };
-    const user = await User.findById(filter)
-    res.status(200).json(user)
+    const user = await User.findById(filter);
+    res.status(200).json(user);
   },
-  updateUserBasket: async (req,res) => {
-    // creates new order with companyName as ref to user?
-    const newBasket = new Basket ({companyName: req.body.companyName, order: [], dateOfOrder: "", dateRequired : ""})
-    newBasket.save(async (err) => {
-      if (err) {
-        throw err;
-      }
-    })
-
-    const userID = req.params.userID
-    // find, update and respond with user with new currentBasket
-    const filter = { _id: userID};
-    await User.findByIdAndUpdate(userID,  { currentBasketID: newBasket._id });
-    const user = await User.find(filter)
-    res.status(202).json(user)
-    },
-  // Delete: (req, res) => {
-  //   const email = req.body.email
-  //   try {
-  //   const user = User.findOne({ email })
-  //   user.remove()
-  //   res.status(200).json({ message: 'User successfully deleted.' })
-  //   } catch (error) {
-  //   res.status(400).json({ message: error.message })
-  //   }
-  //   }
 };
 
 module.exports = UsersController;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const UsersController = require("../controllers/users");
 
 router.post("/", UsersController.Create);
-router.get("/", UsersController.getAll);
 router.get("/:userID", UsersController.getUserByID);
-router.put("/:userID", UsersController.updateUserBasket);
-// router.delete("/", UsersController.Delete);
 
 module.exports = router;
