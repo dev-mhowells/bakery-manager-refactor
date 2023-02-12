066716987edf27f3b447caaa045b08c72b50514f
Mike <dev.mhowells@gmail.com>
2023-02-12 12:14:44 +0000

    changes to styling of modal popup in Bakery home page

--- frontend/src/BakeryHome.js
+++ frontend/src/BakeryHome.js
@@ -48,13 +48,8 @@ export default function BakeryHome() {
             >
               ✕
             </label>
-            <h3 className="text-lg font-bold">
-              Congratulations random Internet user!
-            </h3>
-            <p className="py-4">
-              You've been selected for a chance to get one year of subscription
-              to use Wikipedia for free!
-            </p>
+            <h3 className="text-lg font-bold">Upcoming orders:</h3>
+            <br></br>
 
             <div class="overflow-x-auto">
               <table class="table table-compact w-full">
